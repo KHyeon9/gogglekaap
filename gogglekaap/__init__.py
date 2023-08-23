bfee913fceba869610d13aba05db5e75d7d3a8aa
KHyeon9 <mrk19967@gmail.com>
2023-08-23 19:29:23 +0900

    Flask-wtf로 로그인, 회원가입 레이아웃 생성

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -20,18 +20,50 @@ def create_app():
     def index():
         return render_template("index.html")
 
-    @app.route('/auth/login')
+    @app.route('/auth/login', methods=['GET', 'POST'])
     def login():
         form = LoginForm()
+
+        if form.validate_on_submit(): # method가 POST이고 validator가 ok인 경우
+            # TODO
+            # 1) 유저 조회
+            # 2) 유저 존재 확인
+            # 3) 유저 없으면 생성(회원 가입 페이지로)
+            # 4) 로그인 유지(세션)
+            user_id = form.data.get("user_id")
+            password = form.data.get("password")
+
+            return f"{user_id}, {password}"
+        else:
+            # TODO: Error
+            pass
+
         return render_template('login.html', form=form)
 
     @app.route('/auth/logout')
     def logout():
         return 'logout'
 
-    @app.route('/auth/register')
+    @app.route('/auth/register', methods=['GET', 'POST'])
     def register():
         form = RegisterForm()
+
+        if form.validate_on_submit(): # method가 POST이고 validator가 ok인 경우
+            # TODO
+            # 1) 유저 조회
+            # 2) 유저 존재 확인
+            # 3) 유저 없으면 생성
+            # 4) 로그인 유지(세션)
+            user_id = form.data.get("user_id")
+            user_name = form.data.get("user_name")
+            password = form.data.get("password")
+            repassword = form.data.get("repassword")
+
+            return f"{user_id}, {user_name}, {password}, {repassword}"
+        else:
+            #TODO: Error
+            pass
+
         return render_template('register.html', form=form)
 
     @app.errorhandler(404)
