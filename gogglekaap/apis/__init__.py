fe420d17e8160bd51dd5a983f97ab13a79fbc69a
KHyeon9 <mrk19967@gmail.com>
2023-08-31 18:26:35 +0900

    user 리스트, 단일 조회 apu작성 및 swagger를 통한 확인

--- gogglekaap/apis/__init__.py
+++ gogglekaap/apis/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask_restx import Api
 
+from .user import ns as UserNamespace
+
 blueprint = Blueprint(
     'api',
     __name__,
@@ -16,4 +18,4 @@ api = Api(
 )
 
 
-#TODO: add namespace to Blueprint
+api.add_namespace(UserNamespace)
