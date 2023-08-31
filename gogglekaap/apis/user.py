9cd094b48ea92514fc512953774fddd55a1e852a
KHyeon9 <mrk19967@gmail.com>
2023-08-31 19:50:40 +0900

    User 모델 필드 추가

--- gogglekaap/apis/user.py
+++ gogglekaap/apis/user.py
@@ -1,4 +1,7 @@
-from flask_restx import Namespace, Resource, fields
+from flask import g
+from flask_restx import Namespace, Resource, fields, reqparse
+
+from werkzeug import security
 
 from gogglekaap.models.user import User as UserModel
 
@@ -10,20 +13,49 @@ ns = Namespace(
 user = ns.model(
     'User',
     {
-        'id': fields.Integer(required=True, descriprion='유저 고유 번호'),
-        'user_id': fields.String(required=True, descriprion='유저 아이디'),
-        'user_name': fields.String(required=True, descriprion='유저 이름'),
+        'id': fields.Integer(required=True, description='유저 고유 번호'),
+        'user_id': fields.String(required=True, description='유저 아이디'),
+        'user_name': fields.String(required=True, description='유저 이름'),
+        'created_at': fields.DateTime(required=True, description='생성 날짜'),
     }
 )
 
+post_parser = reqparse.RequestParser()
+post_parser.add_argument('user_id', required=True, help='유저 고유 아이디')
+post_parser.add_argument('user_name', required=True, help='유저 이름')
+post_parser.add_argument('password', required=True, help='유저 비밀번호')
+
 # /api/users
 @ns.route('')
+@ns.response(409, 'User ID is already exists.')
 class UserList(Resource):
     @ns.marshal_list_with(user, skip_none=True)
     def get(self):
         """유저 복수 조회"""
         data = UserModel.query.all()
         return data
+
+    @ns.expect(post_parser)
+    @ns.marshal_list_with(user, skip_none=True)
+    def post(self):
+        """유저 생성"""
+        args = post_parser.parse_args()
+        user_id = args['user_id']
+        user = UserModel.find_one_by_user_id(user_id)
+
+        if user:
+            ns.abort(409)
+
+        user = UserModel(
+            user_id = user_id,
+            user_name = args['user_name'],
+            password = security.generate_password_hash(args['password'])
+        )
+
+        g.db.add(user)
+        g.db.commit()
+
+        return user, 201
 
 # /api/users/<id>
 @ns.route('/<int:id>')
--- gogglekaap/models/user.py
+++ gogglekaap/models/user.py
@@ -1,3 +1,5 @@
+from sqlalchemy import func
+
 from gogglekaap.exts import db
 
 class User(db.Model):
@@ -5,6 +7,7 @@ class User(db.Model):
     user_id = db.Column(db.String(20), unique=True, nullable=False)
     user_name = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime(), server_default=func.now())
 
     @classmethod
     def find_one_by_user_id(cls, user_id):
