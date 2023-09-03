a5d5545b9f77423241635c366c2e92812495d112
KHyeon9 <mrk19967@gmail.com>
2023-09-03 17:07:16 +0900

    세션 적용과 Scope 정의 및 ini 설정

--- gogglekaap/tests/conftest.py
+++ gogglekaap/tests/conftest.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('.')
+import os
 
 from gogglekaap import create_app, db
 from gogglekaap.configs import TestingConfig
@@ -7,7 +8,7 @@ from gogglekaap.models.user import User as UserModel
 
 import pytest
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def user_data():
     yield dict(
         user_id="tester",
@@ -15,7 +16,7 @@ def user_data():
         password="tester"
     )
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app(user_data):
     app = create_app(TestingConfig())
 
@@ -25,10 +26,25 @@ def app(user_data):
         db.session.add(UserModel(**user_data))
         db.session.commit()
 
-    yield app
+        yield app
 
-@pytest.fixture
-def client(app):
+        # 불필요한 DB 정리
+        db.drop_all()
+        db_path = app.config['SQLALCHEMY_DATABASE_URI'].replace(
+            'sqlite://',
+            ''
+        )
+
+        if os.path.isfile(db_path):
+            os.remove(db_path)
+
+
+
+@pytest.fixture(scope='session')
+def client(app, user_data):
     with app.test_client() as client:
+        #NOTE: 세션 입혀주기
+        with client.session_transaction() as session:
+            session['user_id'] = user_data.get('user_id')
         yield client
 
